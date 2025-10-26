9231a4a5b108e47c1a6a1623d6cd708be68a755e
Jose I Toledano Morales <jtoledano@grupoabraxas.com>
2025-10-26 06:25:25 -0600

    feat: implement Clerk authentication in Header and add dashboard menu page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
-import { defineMiddleware } from 'astro:middleware'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server'
 
-export const onRequest = defineMiddleware((context, next) => {
-  // Your middleware logic here
-  const lang = context.request.headers.get('Accept-Language')
-  const longCode = lang?.split(',')[0] || 'en'
-  console.log(`Preferred language: ${longCode}`)
-  if (longCode.startsWith('en-US')) {
-    context.locals.title_md = 'Welcome to our US site!'
-  }else{
-    context.locals.title_md = 'Welcome to our site!'
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)'
+  ]);
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { userId, redirectToSignIn } = auth()
+
+  if (isProtectedRoute(context.request) && !userId) {
+    return redirectToSignIn()
   }
-  return next()
 })
-
