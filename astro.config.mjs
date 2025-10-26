199966cd3e07fd76ce36b96e165f40f61986864f
Jose I Toledano Morales <jtoledano@grupoabraxas.com>
2025-10-25 20:43:24 -0600

    feat: update astro.config.mjs to include node and clerk integrations, and adjust package.json and pnpm-lock.yaml for new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
+  integrations: [react(), clerk()]
 });
